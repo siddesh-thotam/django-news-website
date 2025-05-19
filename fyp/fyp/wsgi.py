73a41af44edd63990f83548c122af00cf79b9c7f
siddesh thotam <siddeshthotam310@gmail.com>
2025-05-19 12:23:32 +0530

    #1

--- fyp/fyp/wsgi.py
+++ fyp/fyp/wsgi.py
@@ -1,13 +1,9 @@
-"""
-WSGI config for fyp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the project root directory to Python path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_root)
 
 from django.core.wsgi import get_wsgi_application
 
